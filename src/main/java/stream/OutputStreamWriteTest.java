51b9d161136476690d89ac16ee48bb54911a9d04
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-09-05 15:29:13 +0900

    Add OutputStreamWriteTest

---
+++ src/main/java/stream/OutputStreamWriteTest.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.stream.IntStream;
+
+public class OutputStreamWriteTest {
+    public static void main(String[] args) {
+        IntStream.range(1, 200)
+                .forEach((num) -> {
+                    System.out.write(num);
+                    System.out.print(num);
+                    System.out.flush();
+                });
+    }
+}
